da7a30207d23100f68b6f962749846c2a8ab7b78
Bernardo Padilla jr <balaiexpressdelivery@gmail.com>
2023-04-17 21:54:03 +0800

    first commit

---
+++ src/main/kotlin/KotlinProgram1.kt
@@ -0,0 +1,105 @@
+fun main(){
+    // Application for Employment
+    var position:String = "Mobile Dev"
+    var availDate:String = "Anytime"
+    var desiredSalary:Int = 50000
+
+    //Personal Information
+    var lastName:String = "Padilla"
+    var firsName:String = "Bernardo Jr"
+    var middleName:String = "Pracillo"
+
+
+    var fullAdd:String = "Santa Rosa"
+    var city:String = "Bayombong"
+    var state:String = "Nueva Vizcaya"
+    var zipCode:Int = 3700
+
+
+    var socialSecNum:Int = 154748000
+    var citizenstatus:Boolean = false
+    var convictionStatus: Boolean = false
+    var submitDrugTest: Boolean = false
+
+
+
+    //EDUCATION
+    var shoolname1:String = "Calitlitan Elementary School"
+    var locshoolname1:String = "Calitlitan Aritao Nueva Vizcaya"
+    var year1:String = "2002"
+    var schoolname2:String = "Aritao National Highschool"
+    var locschoolname2:String = "Aritao Nueva Vizcaya"
+    var year2:String = "2006"
+    var schoolname3:String = "Nueva Vizcaya State University"
+    var locshoolname3:String = "Bayombong Nueva Vizcaya"
+
+
+    //EMPLOYMENT
+    var employer:String = "MDP WORLD INC"
+    var dateemp:String = "January 20,2019"
+    var emplNum:Int = 956664430
+    var emprate:Double = 15000.0
+    var empFullAdd:String = "Cabanatun City Nueva Ecija"
+    var empPosition:String = "Marketing Manager"
+
+    //Reference
+    var refName:String = "Arwin Panaligan"
+    var refTitle:String = "CEO"
+    var refCampany:String = "MDPWORLD INC"
+    var refContact:String = "09557225420"
+
+    println("Position You Are Applying for: $position")
+    println("Desired Salary: $desiredSalary")
+    println("Date Available For Work: $availDate")
+    println("Last Name : $lastName")
+    println("First Name : $firsName")
+    println("Middle Name : $middleName")
+    println("Address : $fullAdd")
+    println("City : $city")
+    println("State : $state")
+    println("Zipcode : $zipCode")
+    println(" ")
+    println(" ")
+    println("Social Security number : $socialSecNum")
+    println("Are you US Citizen? : $citizenstatus  ")
+    println("Have you ever been convicted of a felony : $convictionStatus")
+    println("If selected for employment are you willing to pre-employment drug screening test ? : $submitDrugTest")
+    println(" ")
+    println("EDUCATION")
+    println("Elementary School")
+    println("School Name : $shoolname1")
+    println("School Location : $locshoolname1")
+    println("Year Attended : $year1")
+    println("High School")
+    println("School Name : $schoolname2")
+    println("School Location : $locschoolname2")
+    println("Year Attended : $year2")
+    println("College")
+    println("School Name : $schoolname3")
+    println("School Location : $locshoolname3")
+    println(" ")
+    //Employment
+    println("EMPLOYMENT")
+    println("Employer: $employer")
+    println("Date of Employment : $dateemp")
+    println("Work Phone : $emplNum")
+    println("Address : $empFullAdd")
+    println("Position : $empPosition")
+    println("Pay Rate : $emprate")
+    println(" ")
+    println("REFERENCE")
+    println("Name: $refName")
+    println("Title : $refTitle")
+    println("Company : $refCampany")
+    println("Phone : $refContact")
+
+
+    //
+
+
+
+
+
+
+
+}
